b2b085c4011d6f0431e02097fcb5301bbcfe2049
anthonydroberts <anthonydroberts@cmail.carleton.ca>
2019-02-20 20:17:12 -0500

    v2, BFS done and clean

--- spiderGame.js
+++ spiderGame.js
@@ -1,4 +1,4 @@
-var GRIDSIZE = 10; //set to 30 for 30x30 or 50 for 50x50, or K for KxK
+var GRIDSIZE = 50; //set to 30 for 30x30 or 50 for 50x50, or K for KxK
 var CELLSIZE = 20; //size of each cell, leave as 20 its a good size
 
 // Box width
@@ -60,49 +60,6 @@ function drawEntities(){
 	}
 }
 
-function moveSpider(direction){
-	if (direction == "topRight"){
-		spiderX = spiderX + 1;
-		spiderY = spiderY - 2;
-	}
-	else if (direction == "topLeft"){
-		spiderX = spiderX - 1;
-		spiderY = spiderY - 2;
-	}
-	else if (direction == "left"){
-		spiderX = spiderX - 1;
-	}
-	else if (direction == "down"){
-		spiderY = spiderY + 1;
-	}
-	else if (direction == "right"){
-		spiderX = spiderX + 1;
-	}
-	else if (direction == "bottomRight"){
-		spiderX = spiderX + 1;
-		spiderY = spiderY + 1;
-	}
-	else if (direction == "bottomLeft"){
-		spiderX = spiderX - 1;
-		spiderY = spiderY + 1;
-	}
-}
-
-function moveAnt(direction){
-	if (direction == "up"){
-		antY = antY - 1;
-	}
-	else if (direction == "down"){
-		antY = antY + 1;
-	}
-	else if (direction == "left"){
-		antX = antX - 1;
-	}
-	else if (direction == "right"){
-		antX = antX + 1;
-	}
-}
-
 //initialize everything
 function init(){
 	drawBoard();
@@ -118,136 +75,171 @@ function Node(par, data) {
 function computeMoves(searchType){
 	spiderX = Math.floor(Math.random() * GRIDSIZE);
 	spiderY = Math.floor(Math.random() * GRIDSIZE);
-	antX = Math.floor(Math.random() * GRIDSIZE);
-	antY = Math.floor(Math.random() * GRIDSIZE);
-	var num = Math.floor(Math.random() * 4);
-	if(num == 0){
-		antDirection = "left";
-	}
-	else if(num == 1){
-		antDirection = "up";
-	}
-	else if(num == 2){
-		antDirection = "down";
-	}
-	else if(num == 3){
-		antDirection = "right";
+	var antXchange = 0; var antYchange = 0; var spiderXchange = 0; var spiderYchange = 0;
+	
+	//set ant change rate function
+	function newAnt(){
+		antX = Math.floor(Math.random() * GRIDSIZE);
+		antY = Math.floor(Math.random() * GRIDSIZE);
+		
+		var num = Math.floor(Math.random() * 8);
+		if(num == 0){//up
+			antYchange = -1;
+		}
+		else if( num == 1){//topright
+			antYchange = -1;
+			antXchange = 1;
+		}
+		else if( num == 2){//right
+			antXchange = 1;
+		}
+		else if( num == 3){//bottomright
+			antXchange = 1;
+			antYchange = 1;
+		}
+		else if( num == 4){//down
+			antYchange = 1;
+		}
+		else if( num == 5){//bottomleft
+			antXchange = -1;
+			antYchange = 1;
+		}
+		else if( num == 6){//left
+			antXchange = -1;
+		}
+		else if( num == 7){//topleft
+			antXchange = -1;
+			antYchange = -1;
+		}
+		
 	}
 	
-	if (searchType == "BFS"){
-
-		var NodeList = [];
-		NodeList.push([spiderX,spiderY,antX,antY]);
-		var rootNode = new Node(null,NodeList[0]);
-		//var currNode = new Node(rootNode,rootNode.data);
-		var NodeArray = [];
-		NodeArray.push(rootNode);
-		var VisitedNodeList = [];
-
-		var antXChange = 0, antYChange = 0;
+	//check if state space has already been visited, return false if it hasnt, true if the state space has already been visited
+	function isVisited(VN,CN){
 		
-		if (antDirection == "up"){
-			antYChange = -1;
-		}
-		else if (antDirection == "down"){
-			antYChange = 1;
-		}
-		else if (antDirection == "left"){
-			antXChange = -1;
-		}
-		else if (antDirection == "right"){
-			antXChange = 1;
-		}
+		function arraysEqual(arr1, arr2) {
+			if(arr1.length !== arr2.length)
+				return false;
+			for(var i = arr1.length; i--;) {
+				if(arr1[i] !== arr2[i])
+					return false;
+			}
 
+			return true;
+		}
+		
+		for(var k = 0; k<VN.length;k++){
+			if (arraysEqual(VN[k].data,CN.data) == true){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	if(searchType == "BFS"){
+		newAnt();
+		var rootNode = new Node(null,[spiderX,spiderY,antX,antY]);
+		var Nodes = [];
+		Nodes.push(rootNode);
+		var VisitedNodes = [];
+		var currNode = rootNode;
 		var i = 0;
-		//spiderx,spidery,antx,anty
+		moveList = [];
+		VisitedNodes.push(currNode);
+		
+		
+		
+		function addChild(p){
+			//make sure the new spider coordinates in the new space are not out of bounds
+			if((p.data[0]+spiderXchange < GRIDSIZE) && (p.data[0]+spiderXchange >= 0) && (p.data[1]+spiderYchange < GRIDSIZE) && (p.data[1]+spiderYchange >= 0)){
+				childNode = new Node(p,[p.data[0]+spiderXchange,p.data[1]+spiderYchange,p.data[2]+antXchange,p.data[3]+antYchange]);
+				if(isVisited(VisitedNodes,childNode) == false){ //check if node is already present, if not add it
+					Nodes.push(childNode);
+					VisitedNodes.push(childNode);
+				}
+				else{
+					//console.log("Node Rejected");
+				}
+			}
+		}
+		
 		while(1){
-			var parentNode = NodeArray.shift();
-			if(antX > GRIDSIZE-1 || antY > GRIDSIZE-1 || antX < 0 || antY < 0){
-				antX = Math.floor(Math.random() * GRIDSIZE-1);
-				antY = Math.floor(Math.random() * GRIDSIZE-1);
+			if (Nodes.length==0) break; //quit
+			currNode = Nodes.shift(); //Remove the first element from node list and call it E (currNode)
+			//check if ant coordinates are off grid
+			if((currNode.data[2] > GRIDSIZE-1) || currNode.data[2] < 0 || currNode.data[3] > GRIDSIZE-1 || currNode.data[3] < 0){
+				newAnt();
+				currNode.data[2] = antX;
+				currNode.data[3] = antY;
 			}
 			
-			//if this states spider coordinates equal the ants coordinates
-			console.log(NodeList[0][0],NodeList[0][1],NodeList[0][2],NodeList[0][3])
-			
-			if(NodeList[0][0] == NodeList[0][2] && NodeList[0][1] == NodeList[0][3]){
-				//goal condition met
-				//succesful node is NodeArray[i];
-				var solution = [];
-				//record the move in the succesful node, go to its parent, record its move, go to its parent, etc..
-				var lastNode = new Node(NodeArray.shift(),NodeList[0]);
-				var nodeInSolution = lastNode;
-				//console.log()
-				solution.unshift(lastNode.data);
-				while(nodeInSolution.parent!=null){//nodeInsolution isnt the root node
-					
-					nodeInSolution = nodeInSolution.parent;
-					solution.unshift(nodeInSolution.data);
-					
+			//goal state check
+			console.log(currNode.data[0], currNode.data[1], currNode.data[2], currNode.data[3])
+			if((currNode.data[0] == currNode.data[2]) && (currNode.data[1] == currNode.data[3])){
+				//goal state reached
+				alert("SUCCESS AFTER: " + i);
+				while(currNode.parent!=null){
+					moveList.unshift(currNode.data);
+					currNode = currNode.parent;
 				}
-				console.log(solution);
-				//print out solution
-				moveList = solution;
-				console.log(moveList);
-				alert("SUCCESS AFTER: " + i)
 				break;
 			}
-			
-			
-			if(!VisitedNodeList.includes([NodeList[0][0]+1,NodeList[0][1]-2,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][0]+1 > GRIDSIZE) || (NodeList[0][1]-2 < 0))){
-					var createdNode0 = new Node(parentNode,[NodeList[0][0]+1,NodeList[0][1]-2,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0]+1,NodeList[0][1]-2,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode0);
-				}
-			}
-			if(!VisitedNodeList.includes([NodeList[0][0]-1,NodeList[0][1]-2,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][0]-1 < 0) || (NodeList[0][1]-2 < 0))){
-					var createdNode1 = new Node(parentNode,[NodeList[0][0]-1,NodeList[0][1]-2,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0]-1,NodeList[0][1]-2,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode1);
-				}
-			}
-			if(!VisitedNodeList.includes([NodeList[0][0]-1,NodeList[0][1],NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][0]-1 < 0))){
-					var createdNode2 = new Node(parentNode,[NodeList[0][0]-1,NodeList[0][1],NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0]-1,NodeList[0][1],NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode1);
-				}
-			}
-			if(!VisitedNodeList.includes([NodeList[0][0],NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][1]+1 > GRIDSIZE))){
-					var createdNode3 = new Node(parentNode,[NodeList[0][0],NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0],NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode3);
-				}
-			}
-			if(!VisitedNodeList.includes([NodeList[0][0]+1,NodeList[0][1],NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][0]+1 > GRIDSIZE))){
-					var createdNode4 = new Node(parentNode,[NodeList[0][0]+1,NodeList[0][1],NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0]+1,NodeList[0][1],NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode4);
-				}
-			}
-			if(!VisitedNodeList.includes([NodeList[0][0]+1,NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][0]+1 > GRIDSIZE) || (NodeList[0][1]+1 > GRIDSIZE))){
-					var createdNode5 = new Node(parentNode,[NodeList[0][0]+1,NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0]+1,NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode5);
-				}
-			}
-			if(!VisitedNodeList.includes([NodeList[0][0]-1,NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange])){
-				if(!((NodeList[0][0]-1 < 0) || (NodeList[0][1]+1 > GRIDSIZE))){
-					var createdNode6 = new Node(parentNode,[NodeList[0][0]-1,NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeList.push([NodeList[0][0]-1,NodeList[0][1]+1,NodeList[0][2]+antXChange,NodeList[0][3]+antYChange]);
-					NodeArray.push(createdNode6);
-				}
-			}
+			else{
+				//goal state not reached yet
+				//spider can move in 9 directions, so we will add at most 9 children to this current node representing every space possible
+				
+				//topright1
+				spiderXchange = 1;
+				spiderYchange = -2;
+				addChild(currNode);
+				
+				//topright2
+				spiderXchange = 2;
+				spiderYchange = -1;
+				addChild(currNode);
+				
+				//topleft1
+				spiderXchange = 1;
+				spiderYchange = -2;
+				addChild(currNode);
+				
+				//topleft2
+				spiderXchange = -2;
+				spiderYchange = -1;
+				addChild(currNode);
+				
+				//right
+				spiderXchange = 1;
+				addChild(currNode);
+				
+				//bottomright
+				spiderXchange = 1;
+				spiderYchange = 1;
+				addChild(currNode);
 
-			VisitedNodeList.push([NodeList[0][0],NodeList[0][1],NodeList[0][2],NodeList[0][3]])
-
-			NodeList.shift()
+				//down
+				spiderYchange = 1;
+				addChild(currNode);
+				
+				//bottomleft
+				spiderXchange = -1;
+				spiderYchange = 1;
+				addChild(currNode);
+				
+				//left
+				spiderXchange = -1;
+				addChild(currNode);
+				
+				//apply rule to generate all states
+				//for each state, check if it has been visited
+				//if it has been visited, do not add it to Nodes
+				//check if it is goal node'
+				//if not goal node add to Nodes
+				
+				
+			}
+			
 			i++;
 		}
 		
@@ -255,16 +247,23 @@ function computeMoves(searchType){
 }
 
 function run(){
-
+	spiderX = -2;
+	spiderY = -2;
+	antX = -3;
+	antY = -3;
+	drawBoard(); //clear and redraw all the entities and board for a clean start every iteration
+	drawEntities();
+	
 	computeMoves("BFS");
 	
 //setInterval is used instead of a for loop because the delay is needed for the user to see the changes
 	var i = 0;
+	console.log("Solution set:");
+	console.log(moveList);
 	setInterval(function() {
 		if (i < moveList.length) {
 			context.clearRect(spiderX*CELLSIZE+p,spiderY*CELLSIZE+p,CELLSIZE,CELLSIZE);
 			context.clearRect(antX*CELLSIZE+p,antY*CELLSIZE+p,CELLSIZE,CELLSIZE);
-			console.log(moveList);
 			spiderX = moveList[i][0];
 			spiderY = moveList[i][1];
 			antX = moveList[i][2];
